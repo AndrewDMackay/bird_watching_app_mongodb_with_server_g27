85911a82120c707f5117d5b0462bc9dd35e60587
Andrew Mackay <andrewmackayd@gmail.com>
2021-10-19 15:16:43 +0100

    3rd commit main

--- server/helpers/create_router.js
+++ server/helpers/create_router.js
@@ -19,8 +19,35 @@ const createRouter = function (collection) {
         })    
     });
 
-    
+
     // SHOW, get a sighting from MongoDB, via an ID, and serve as JSON..
+
+    router.get('/:id', (req, res) => {
+        const id = req.params.id;
+        collection.findOne({ _id: ObjectID(id)})
+        .then((doc) => res.json(doc))
+        .catch((err) => {
+            console.error(err);
+            res.status(500);
+            res.json({ status: 500, error: err })
+        })   
+    });
+
+
+    // CREATE, Post new sighting, and persist to the database..
+
+    // Note.. To parse the req.'body' requires, 'app.use(express.json());' in the head of your server..
+
+    router.post('/', (req, res) => {
+        const newData = req.body;
+        collection.insertOne(newData)
+        .then((result) => {res.json(result.ops[0])})
+        .catch((err) => {
+            console.error(err);
+            res.status(500);
+            res.json({ status: 500, error: err })
+        })    
+    });
 
 
 return router;
